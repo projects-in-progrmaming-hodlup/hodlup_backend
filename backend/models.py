347d59cb94440fccc3271b0db3704dbe1c299848
Molly Zhang <yz9753@nyu.edu>
2024-12-10 11:32:15 -0500

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,7 @@
 # backend/models.py
 from sqlalchemy import Column, Integer, String, Float, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
-from database import Base
+from backend.database import Base
 from datetime import datetime
 
 class User(Base):
